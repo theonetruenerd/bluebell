048f3f566d6891cfa345189f12d1eef5da522a2e
TarunChapman <tarun@optigenomics.bio>
2025-05-28 16:36:56 +0100

    Added missing semicolons

--- generators.ts
+++ generators.ts
@@ -1,14 +1,14 @@
 Blockly.Python['import_bluebell'] = function (block) {
     const code = 'from bluebell import *\n';
     return code
-}
+};
 
 Blockly.Python['aspirate'] = function (block) {
     const code = 'print(\"Aspirating\")';
     return code
-}
+};
 
 Blockly.Python['dispense'] = function (block) {
     const code = 'print(\"Dispense\")';
     return code
-}
+};
