5be80f64ec7deb2c5aa520e3c533dfeb0e702136
TarunChapman <tarun@optigenomics.bio>
2025-05-28 16:38:11 +0100

    Removed class from bluebell lib

--- bluebell.py
+++ bluebell.py
@@ -1,14 +1,10 @@
 # Bluebell library
 
-class bluebell:
-    def __init__(self):
-        pass
+def aspirate(volume):
+    print(f"Aspirating {volume}uL")
+    pass
 
-    def aspirate(self, volume):
-        print(f"Aspirating {volume}uL")
-        pass
-
-    def dispense(self, volume):
-        print(f"Dispensing {volume}uL")
-        pass
+def dispense(volume):
+    print(f"Dispensing {volume}uL")
+    pass
 
