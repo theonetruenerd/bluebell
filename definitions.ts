00350c82b2630a4da1309aefd5ea9e5da7bf1a05
TarunChapman <tarun@optigenomics.bio>
2025-05-28 16:27:44 +0100

    Adjusted config, changed var to const in generators.ts and definitions.ts

--- definitions.ts
+++ definitions.ts
@@ -1,10 +1,12 @@
+const bluebellColour = "#1BE7FF";
+
 Blockly.Blocks['import_bluebell'] = {
     init: function () {
         this.appendDummyInput().appendField("from bluebell import *");
         this.setPreviousStatement(true, null);
         this.setNextStatement(true, null);
-        this.setColour("#a2a2d0");
+        this.setColour(bluebellColour);
         this.setTooltip("Imports bluebell library.");
         this.setHelpUri("");
-    }
+    },
 };
